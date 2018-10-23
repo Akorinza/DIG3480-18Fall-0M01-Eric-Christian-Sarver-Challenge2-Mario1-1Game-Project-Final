f787c99b2bdb9c082b615713872c04e2d036bf6b
Akorinza <42954528+Akorinza@users.noreply.github.com>
2018-10-23 02:55:05 -0400

    Individually Uploaded Scripts

---
+++ Scripts/FlagpoleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlagpoleController : MonoBehaviour {
+
+    public Text winText;
+
+	// Use this for initialization
+	void Start () {
+
+        winText.text = "";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Flagpole"))
+            winText.text = "You Win!";
+
+    }
+
+}
